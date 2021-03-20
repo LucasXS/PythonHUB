8337eb84366d5422814c579d0c8151b652b137a3
lucas <contato_lucassantos@outlook.com>
2021-03-20 00:24:50 -0300

    Exercício de fixação - O que sai na tela? #1

--- PyhtonHUB-Instragram/WhatOutput/QuestionWhatOutput.py
+++ PyhtonHUB-Instragram/WhatOutput/QuestionWhatOutput.py
@@ -3,7 +3,6 @@ import math
 # What will be the output of the following
 # Python Code?#
 # ---------------------------------------- #
-
 my_num = 15.2
 print(math.floor(my_num), end=" ")  # arredonda para baixo
 print(math.ceil(my_num))            # mostra o próximo número
