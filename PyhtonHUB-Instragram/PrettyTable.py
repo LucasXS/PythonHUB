8311d9ae2e00524a131ce2bab936a7c49bd0ad75
lucas <contato_lucassantos@outlook.com>
2021-03-20 00:21:55 -0300

    Criando uma tabela em Python
    Criando de forma super simples uma tabela em Python

--- PyhtonHUB-Instragram/PrettyTable.py
+++ PyhtonHUB-Instragram/PrettyTable.py
@@ -1,4 +1,5 @@
 from prettytable import PrettyTable
+
 # Specify the Column Names while initializing the Table
 myTable = PrettyTable(["Student Name", "Last Name", "Course", "Percentage of grades"])
 # add rows
@@ -7,5 +8,6 @@ myTable.add_row(["Michael", "Polidoro", "Sistemas", "85.66 %"])
 myTable.add_row(["Murilo", "Doido", "Banco de dados", "75.80 %"])
 myTable.add_row(["Henrique", "Neves", "Analytics", "80.75"])
 print(myTable)
+
 
 
