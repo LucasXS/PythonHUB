0607d63c2ede8c928f94a8602b20179428bf43a2
lucas <contato_lucassantos@outlook.com>
2021-03-20 14:50:03 -0300

    Update botWhatsapp.py
    Explicações do funcionamento do codigo

--- RandonModules/botWhatsapp.py
+++ RandonModules/botWhatsapp.py
@@ -1,12 +1,14 @@
+"""Precisei mudar dentro do mainfunctions uma das execessões na linha 292
+if tyme >= 60:
+         raise Warning("INTERNET IS SLOW, extraction of information might take longer time")
+Onde está >= 60 estava 5 e isso estava ocorrendo o erro indicado na exceção"""
+
 import pywhatkit
 
-lista_numeros = ['+55119SEUNUMERO', '+55119SEUNUMERO2']
-mensagem = "Olá, Gostaria de ganhar um premio agora?"
+lista_numeros = ['+5511997484170']
+mensagem = "Salveeee cachorro"
 
 for numero in lista_numeros:    # para cada nýmero que existir em uma lista de números
-    pywhatkit.sendwhatmsg(numero, mensagem, 0,3)    # biblioteca chama a função e enviar x msg.
+    # substituia o 11 pela hora atual e o 25 pelo próximo minuto do seu relogio
+    pywhatkit.sendwhatmsg(numero, mensagem, 11,25)    # biblioteca chama a função e enviar x msg.
 
-# Precisei mudar dentro do mainfunctions uma das execessões linha 292
-# if tyme >= 60:
-#         raise Warning("INTERNET IS SLOW, extraction of information might take longer time")
-# Onde está >= 60 estava 5 e isso estava ocorrendo o erro indicado no exceção
