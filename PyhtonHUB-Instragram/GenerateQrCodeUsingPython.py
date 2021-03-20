74e0821c2a5980c0451561bf60bf6a33a71020e6
lucas <contato_lucassantos@outlook.com>
2021-03-20 00:19:38 -0300

    Gerarando um QRCODE com Python
    Como gerar um simples QRCODE em Python

--- PyhtonHUB-Instragram/GenerateQrCodeUsingPython.py
+++ PyhtonHUB-Instragram/GenerateQrCodeUsingPython.py
@@ -1,14 +1,10 @@
 # pip install pyqrcode
-# pip install pypng
-
 # read more: https://pypi.org/project/PyQRCode/
 
 import pyqrcode
-import png
 
 qr = "https://python.org.br/"    # paste any url here
 url = pyqrcode.create(qr)
-# url.png('Desktop\\qr.png', scale=8)
 
 print(url.terminal())
 
