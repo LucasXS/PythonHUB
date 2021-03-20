7e3858cdb853aabdcedc073c52c685a8897d05d0
lucas <contato_lucassantos@outlook.com>
2021-03-20 00:13:49 -0300

    list, tuple, set

--- PyhtonHUB-Instragram/AmazingPythonTricks.py
+++ PyhtonHUB-Instragram/AmazingPythonTricks.py
@@ -1,13 +1,15 @@
 # Multi Assigments
+
 a = [1, 2, 3]   # list
 b = (4, 5, 6)   # tuple
 c = {7, 8, 9}   # set
 
+#
 p, q, r = a
 s, t, u = b
 x, y, z = c
 
-
+#
 print(p, q, r)
 print(s, t, u)
-print(x, y, z)  # pq essa inverte? testar com 5
+print(x, y, z)  # testar com mais números
